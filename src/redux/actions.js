f5a34a2eb168e76241e6788a8bfc9df1ca48056f
RamOmri <Omriyram@gmail.com>
2021-07-17 22:37:40 +0300

    final comments

--- src/redux/actions.js
+++ src/redux/actions.js
@@ -8,18 +8,22 @@ import {
   IS_AGAINST_ALG,
 } from './types';
 
+//set the grid size on the settings screen
 export const set_gridsize = size => ({
   type: SET_GRIDSIZE,
   data: size,
 });
+//add cells to the board map
 export const add_column = cells => ({
   type: ADD_COLUMN,
   data: cells,
 });
+//set which player is currently playing
 export const set_current_player = player => ({
   type: SET_CURRENT_PLAYER,
   data: player,
 });
+//Update the map when someone makes a move
 export const update_board_map = obj => ({
   type: MODIFY_BOARD_MAP,
   data: obj,
@@ -28,9 +32,11 @@ export const set_winner = winner => ({
   type: SET_WINNER,
   data: winner,
 });
+//Resets the store
 export const reset_game = () => ({
   type: RESET_GAME,
 });
+//Determine whether or not the user is playing against the algorithm. 
 export const set_is_against_alg = (isAlg) => ({
   type: IS_AGAINST_ALG,
   data: isAlg
